16691dc5d68db872699a9eb9fb92608a02142ab2
cochraef <cochraef@rose-hulman.edu>
2021-10-18 22:24:56 -0400

    Test code

--- test/derrivatives.lua
+++ test/derrivatives.lua
@@ -19,9 +19,11 @@ end
 local a = DD(SymbolExpression("x"), SymbolExpression("x") * SymbolExpression("y"))
 local b = DD(SymbolExpression("x"), Integer(3) * SymbolExpression("x") ^ Integer(2) + Integer(2) * SymbolExpression("x") + Integer(6))
 local c = DD(SymbolExpression("x"), E ^ SymbolExpression("x"))
+local d = DD(SymbolExpression("x"), FunctionExpression("f", {SymbolExpression("x") ^ Integer(2)}))
 
 print("Testing Differentiation...")
 test(a, "(d/dx (x * y))")
 test(a:autosimplify(), "y", a)
 test(b:autosimplify(), "(2 + (6 * x))", b)
 test(c:autosimplify(), "(e ^ x)", c)
+test(d:autosimplify(), "((2 * x) * f'((x ^ 2)))", d)
