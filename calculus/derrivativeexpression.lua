87faa12ea38259b6c9f20c0a89e0b6cd53f03d4d
timothy-all <timothy.all@gmail.com>
2022-05-14 16:36:09 -0400

    new diffexpression
    natively handles higher order derivatives

--- calculus/derrivativeexpression.lua
+++ calculus/derrivativeexpression.lua
@@ -5,6 +5,9 @@
 DerrivativeExpression = {}
 __DerrivativeExpression = {}
 
+DiffExpression = {}
+__DiffExpression = {}
+
 ----------------------------
 -- Instance functionality --
 ----------------------------
@@ -32,6 +35,57 @@ function DerrivativeExpression:new(expression, symbol)
             return false
         end
         return a.symbol == b.symbol and a.expression == b.expression
+    end
+    o = setmetatable(o, __o)
+
+    return o
+end
+
+-- Creates a new derivative operation with the given symbols and expression.
+--- @param expression Expression 
+--- @param symbols Symbols 
+--- @return DiffExpression 
+function DiffExpression:new(expression,symbols)
+    local o = {}
+    local __o = Copy(__ExpressionOperations)
+
+    o.symbols = Copy(symbols)
+    o.degree   = #o.symbols
+    o.expression = Copy(expression)
+
+    __o.__tostring = function(a) 
+        local varlist = '(d'
+        if a.degree == 1 then 
+            varlist = varlist .. '/d' .. tostring(a.symbols[1]) .. " " .. tostring(a.expression) .. ')'
+        end
+        if a.degree > 1 then 
+            varlist = varlist .. '^' .. tostring(a.degree) .. '/'
+            for index, var in ipairs(a.symbols) do 
+                varlist = varlist .. 'd' .. tostring(var)
+            end 
+            varlist = varlist .. " " .. tostring(a.expression) .. ')' 
+        end 
+        return varlist 
+    end 
+
+    __o.__index = DiffExpression
+    __o.__eq = function(a, b)
+        -- This shouldn't be needed, since __eq should only fire if both metamethods have the same function, but for some reason Lua always rungs this anyway
+        if not b:type() == DiffExpression then
+            return false
+        end
+        if a.expression ~= b.expression then 
+            return false
+        end
+        local loc = 1
+        while a.symbols[loc] or b.symbols[loc] do
+            if not a.symbols[loc] or not b.symbols[loc] or
+                (a.symbols[loc] ~= b.symbols[loc]) then
+                return false
+            end
+            loc = loc + 1
+        end
+        return true 
     end
     o = setmetatable(o, __o)
 
@@ -172,9 +226,23 @@ function DerrivativeExpression:autosimplify()
     return self
 end
 
+--- @return Expression
+function DiffExpression:autosimplify()
+    local simplified = self.expression:autosimplify()
+
+    for index,var in ipairs(self.symbols) do 
+        simplified = DerrivativeExpression(simplified,var):autosimplify()
+    end
+    return simplified
+end
 
 --- @return table<number, Expression>
 function DerrivativeExpression:subexpressions()
+    return {self.expression}
+end
+
+--- @return table<number, Expression>
+function DiffExpression:subexpressions()
     return {self.expression}
 end
 
@@ -182,6 +250,12 @@ end
 --- @return DerrivativeExpression
 function DerrivativeExpression:setsubexpressions(subexpressions)
     return DerrivativeExpression(subexpressions[1], self.symbol)
+end
+
+--- @param subexpressions table<number, Expression>
+--- @return DiffExpression
+function DiffExpression:setsubexpressions(subexpressions)
+    return DiffExpression(subexpressions[1], self.symbols)
 end
 
 -- function DerrivativeExpression:freeof(symbol)
@@ -218,11 +292,68 @@ function DerrivativeExpression:order(other)
     return self.expression:order(other.expression)
 end
 
+--- @param other Expression
+--- @return boolean
+function DiffExpression:order(other)
+    if other:type() == IntegralExpression then
+        return true
+    end
+
+    if other:type() ~= DiffExpression then
+        return false
+    end
+
+    if self.degree > other.degree then
+        return false
+    end
+
+    if self.degree < other.degree then 
+        return true
+    end
+
+    return self.expression:order(other.expression)
+end
+
 --- @return string
 function DerrivativeExpression:tolatex()
     return '\\frac{d}{d' .. self.symbol:tolatex() .. '}(' .. self.expression:tolatex() .. ')'
 end
 
+function DiffExpression:tolatex()
+    local varlist = '\\frac{'
+    if self.degree == 1 then
+        varlist = varlist .. '\\mathrm{d}}{\\mathrm{d}' .. self.symbols[1]:tolatex() .. '}\\left(' .. self.expression:tolatex() .. '\\right)'
+    end
+    if self.degree > 1 then       
+        local cvarlist = {}
+        local count = 1
+        for index, var in ipairs(self.symbols) do 
+            if var == self.symbols[index+1] then 
+                count = count + 1
+            else
+                table.insert(cvarlist,{var,count})
+                count = 1
+            end
+        end   
+        if #cvarlist == 1 then 
+            varlist = varlist .. '\\mathrm{d}^{' .. self.degree .. '}}{' .. '\\mathrm{d}' .. cvarlist[1][1]:tolatex() .. '^{' .. self.degree .. '}'
+        end
+        if #cvarlist > 1 then 
+            varlist = varlist .. '\\partial^{' .. self.degree .. '}}{' 
+            for index, varnum in ipairs(cvarlist) do 
+                var = cvarlist[#cvarlist - index+1][1]
+                num = cvarlist[#cvarlist - index+1][2]
+                if num == 1 then 
+                    varlist = varlist .. '\\partial ' .. var:tolatex()
+                else 
+                    varlist = varlist .. '\\partial ' .. var:tolatex() .. '^{' .. num .. '}'
+                end
+            end
+        end 
+        varlist = varlist .. '} \\left(' .. self.expression:tolatex() .. '\\right)'
+    end 
+    return varlist 
+end 
 
 -----------------
 -- Inheritance --
@@ -231,9 +362,29 @@ __DerrivativeExpression.__index = CompoundExpression
 __DerrivativeExpression.__call = DerrivativeExpression.new
 DerrivativeExpression = setmetatable(DerrivativeExpression, __DerrivativeExpression)
 
+__DiffExpression.__index = CompoundExpression
+__DiffExpression.__call = DiffExpression.new
+DiffExpression = setmetatable(DiffExpression, __DiffExpression)
+
 ----------------------
 -- Static constants --
 ----------------------
 DD = function(expression, symbol)
     return DerrivativeExpression(expression, symbol)
+end
+
+diff = function(expression,...)
+    local symbols = {}
+    for i = 1, select("#",...) do 
+        local var = select(i,...)
+        if #var == 0 then 
+            table.insert(symbols,var) 
+        end
+        if #var > 0 then 
+            for index=1, RR(var[2]) do 
+                table.insert(symbols,var[1])
+            end
+        end
+    end
+    return DiffExpression(expression,symbols)
 end
