7b1d60243ee87913f78c7a83e8f476a58655c8b4
timothy-all <timothy.all@gmail.com>
2022-06-13 08:12:05 -0400

    Update logarithm.lua
    more fixes to logarith:order()

--- algebra/logarithm.lua
+++ algebra/logarithm.lua
@@ -91,12 +91,16 @@ function Logarithm:order(other)
 
     if other:type() == BinaryOperation then 
         if other.operation == BinaryOperation.ADD or other.operation == BinaryOperation.MUL then 
-            return BinaryOperation(other.operation,{self.expression}):order(other)
+            return BinaryOperation(other.operation,{self}):order(other)
         end
 
         if other.operation == BinaryOperation.POW then 
-            return (self.expression ^ Integer.one()):order(other)
+            return (self ^ Integer.one()):order(other)
         end
+    end
+
+    if other:type() == TrigExpression then 
+        return self.expression:order(other.expression)
     end
 
     return true
