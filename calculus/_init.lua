755f509a8352b07ecd59406605b843589a079de0
cochraef <cochraef@rose-hulman.edu>
2022-08-23 17:43:10 -0400

    deleted old copies of the CAS:

--- calculus/_init.lua
+++
@@ -1,6 +0,0 @@
--- Loads calculus files in the correct order.
-require("algebra._init")
-
-require("calculus.derivativeexpression")
-require("calculus.integralexpression")
-require("calculus.diffexpression")
