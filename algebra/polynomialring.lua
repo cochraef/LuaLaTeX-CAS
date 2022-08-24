345775cd3dbf5dd645f722042d48350a14c65c39
timothy-all <timothy.all@gmail.com>
2022-08-24 09:54:40 -0400

    Squashed 'texmf/tex/latex/luacas/' changes from c1a544b..e07c63f
    e07c63f Update polynomialring.lua
    399bd5a Update polynomialring.lua
    
    git-subtree-dir: texmf/tex/latex/luacas
    git-subtree-split: e07c63f4eb1132c289d1184c9fb1d6e5aea26c71

--- algebra/polynomialring.lua
+++ algebra/polynomialring.lua
@@ -269,6 +269,15 @@ function PolynomialRing:tolatex()
                     goto continue
                 end
             end
+            if coeff == Integer(-1) then 
+                if loc == 0 then 
+                    out = out .. "-" .. coeff:neg():tolatex()
+                    goto skip
+                else
+                    out = out .. "-"
+                    goto continue
+                end
+            end
             if coeff < Integer.zero() then 
                 out = out .. "-" .. coeff:neg():tolatex()
             end 
@@ -616,7 +625,7 @@ end
 
 -- This expression is free of a symbol if and only if the symbol is not the symbol used to create the ring.
 function PolynomialRing:freeof(symbol)
-    return ~(symbol.symbol==self.symbol)
+    return symbol.symbol ~= self.symbol
 end
 
 -- Replaces each expression in the map with its value.
